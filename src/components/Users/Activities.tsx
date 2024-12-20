403de45de077e4f1c80bebc8b506c3d5a97857aa
seliwanoff <olagunjuselim2019@gmail.com>
2024-12-20 08:31:06 +0100

    aser act added

--- src/components/Users/Activities.tsx
+++ src/components/Users/Activities.tsx
@@ -19,32 +19,38 @@ const Activities: React.FC<CustomDatePickerProps> = ({ reports }) => {
             View all
           </Link>
         </div>
-        <div className="flex flex-col gap-[20px] mt-4">
-          {reports &&
-            reports?.map((item: any, index: any) => (
-              <div className="w-full flex justify-between items-center">
-                <div className="flex gap-3 items-center">
-                  <img
-                    src={
-                      item.display_picture
-                        ? `https://cardri.s3.eu-west-1.amazonaws.com/${item.display_picture}`
-                        : avater
-                    }
-                    alt=""
-                    className="h-8 w-8 rounded-full object-contain"
-                  />
-                  <div>
-                    <span className="block text-[#344054] font-medium text-[14px] leading-6 font-Inter">
-                      {item.name}
-                    </span>
-                    <span className="block text-[#475467] font-normal text-[14px] leading-6 font-Inter">
-                      Uploaded {item.title}
-                    </span>
+        {reports && reports.length > 0 ? (
+          <div className="flex flex-col gap-[20px] mt-4">
+            {reports &&
+              reports?.map((item: any, index: any) => (
+                <div className="w-full flex justify-between items-center">
+                  <div className="flex gap-3 items-center">
+                    <img
+                      src={
+                        item.display_picture
+                          ? `https://cardri.s3.eu-west-1.amazonaws.com/${item.display_picture}`
+                          : avater
+                      }
+                      alt=""
+                      className="h-8 w-8 rounded-full object-contain"
+                    />
+                    <div>
+                      <span className="block text-[#344054] font-medium text-[14px] leading-6 font-Inter">
+                        {item.name}
+                      </span>
+                      <span className="block text-[#475467] font-normal text-[14px] leading-6 font-Inter">
+                        Uploaded {item.title}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-        </div>
+              ))}
+          </div>
+        ) : (
+          <div className="font-Inter font-medium text-gray-700 text-[14px] text-center w-full mt-4">
+            No record found.
+          </div>
+        )}
       </div>
     </div>
   );
