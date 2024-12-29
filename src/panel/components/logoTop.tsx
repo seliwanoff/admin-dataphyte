2d6b2273567a843aedd92d2d446de07253c847e1
seliwanoff <olagunjuselim2019@gmail.com>
2024-12-29 19:33:44 +0100

    title changed

--- src/panel/components/logoTop.tsx
+++ src/panel/components/logoTop.tsx
@@ -1,7 +1,8 @@
 const LogoTop = () => {
+  const title = process.env.REACT_APP_TITLE;
   return (
     <h2 className="text-[#7F55DA]  text-2xl leading-6 font-semibold">
-      Dataphyte
+      {title}
     </h2>
   );
 };
